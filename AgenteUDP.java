0d3b528643b1c7a94e7555253dc6f3cff6dcdd2b
Nuno Leite <a70132@alunos.uminho.pt>
2018-03-13 10:40:55 +0000

    Ainda da io exception

--- AgenteUDP.java
+++ AgenteUDP.java
@@ -1,8 +1,37 @@
-
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.net.DatagramPacket;
 
 public class AgenteUDP {
 
-	public AgenteUDP() {
-		
-	}
- }
+	public static void main(String[] args) {
+		MulticastSocket ms;
+		InetAddress group;
+		int porta = 8888;
+		try {
+			ms = new MulticastSocket(porta);
+			group = InetAddress.getByName("239.8.8.8");
+			ms.joinGroup(group);
+
+			String msg = "Agente sending to Monitor";
+			DatagramPacket dp = new DatagramPacket(msg.getBytes(),msg.length(),group,8888);
+			ms.send(dp);
+
+			byte[] buf = new byte[1000];
+			DatagramPacket recv = new DatagramPacket(buf,buf.length);
+			ms.receive(recv);
+			String sentence = new String(recv.getData());
+			System.out.println(sentence);
+
+			} catch(UnknownHostException ex) {
+				System.out.println("unknown host exception");
+			} catch(IOException ex) {
+				System.out.println("ioexception");
+			}
+	} 
+
+
+
+ }		
--- MonitorUDP.java
+++ MonitorUDP.java
@@ -1,7 +1,36 @@
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.DatagramPacket;
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 
 public class MonitorUDP {
 
-	public MonitorUDP() {
-		
-	}
+		public static void main(String[] args) {
+			InetAddress group;
+			MulticastSocket ms;
+			int porta = 8888;
+			try {
+				ms = new MulticastSocket(porta);
+				group = InetAddress.getByName("239.8.8.8");
+				ms.joinGroup(group);
+			
+
+			String msg = "Monitor sending to Agente";
+			DatagramPacket dp = new DatagramPacket(msg.getBytes(),msg.length(),group,8888);
+			ms.send(dp);
+
+			byte[] buf = new byte[1000];
+			DatagramPacket recv = new DatagramPacket(buf,buf.length);
+			ms.receive(recv);
+			String sentence = new String(recv.getData());
+			System.out.println(sentence);
+
+			} catch(UnknownHostException ex) {
+				System.out.println("unknown host exception");
+			} catch(IOException ex) {
+				System.out.println("io exception");
+			}
+		}
 }
